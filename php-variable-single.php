9f48a80ff71e2ae2caaa4020ba1319980ef3356a
kreatifproduk568 <kreatifproduk568@gmail.com>
2025-02-12 10:52:43 +0700

    first commit

---
+++ php-variable-single.php
@@ -0,0 +1,14 @@
+<!-- Variabel single 
+  yaitu variabel yang hanya bisa menyimpan satu nilai tertentu dalam satu waktu -->
+<?php 
+//   deklarasi variabel
+$x;
+
+// deklarasi variabel sekalian memberi nilai awal 
+$y = 5;
+$nama = "Budi";
+
+//mencetak isi variabel
+print $y;
+print $nama;
+?>
